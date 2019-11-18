01105db972c5d6332e73173f9c094a21ef01ebd3
LiangTian168 <56328415+LiangTian168@users.noreply.github.com>
2019-11-18 13:06:06 +0800

    Add files via upload

---
+++ Data/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+    class Program 
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //DataProcessing dataProcessing = new DataProcessing();
+            //dataProcessing.Array();
+            //dataProcessing.Aggregate();
+
+            MyClass myClass = new MyClass("CREATE OBJECT");
+            Console.WriteLine("myClass created.");
+            for(int i=-1;i<=0;i++)
+            {
+                try
+                {
+                    Console.WriteLine($"\nAttemping to assign {i}to myobj.val...");
+                    myClass.IntVal = i;
+                    Console.WriteLine($"Value {myClass.IntVal} assign to myObj.intVal.");
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Exception {e.GetType().FullName}");
+                    Console.WriteLine($"Message:\n{e.Message}");
+
+                }
+
+            }
+            Console.WriteLine($"\nOutputting {myClass.ToString()}");
+            Console.WriteLine($"output.");
+            Console.WriteLine( MyClass.StaticInt ); 
+            MyBaseClass myBaseClass = new MyBaseClass();
+            myBaseClass.Test();
+            MyDervedClass myDervedClass = new MyDervedClass();
+            myDervedClass.Test();
+            TwoSub twoSub = new TwoSub();
+            twoSub.Test();
+            
+        }
+    }
+}
